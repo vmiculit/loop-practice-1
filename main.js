d8853e8997359222fab87e9ee1c1b7d372397a52
Valentin Miculit <valsbox@gmail.com>
2016-01-18 11:26:23 -0700

    done

---
+++ main.js
@@ -0,0 +1,60 @@
+// DEMO PROBLEM
+
+console.log("DEMO PROBLEM");
+for(var i = 0; i < 5; i++){
+    console.log(i);
+}
+
+// PROBLEM 1
+
+console.log("PROBLEM 1");
+for (var i = 0; i <= 500; i += 100){
+	console.log(i);
+}
+
+// PROBLEM 2
+
+console.log("PROBLEM 2");
+for (var i = 1; i <= 64; i *= 2){
+	console.log(i);
+}
+
+// PROBLEM 3
+
+console.log("PROBLEM 3");
+for (var i = 1; i <= 3; i += 1){
+	console.log(i);
+	console.log(i);
+	console.log(i);
+}
+
+// PROBLEM 4
+
+console.log("PROBLEM 4");
+for (var i = 0; i <= 10; i += 2){
+	console.log(i);
+}
+
+// PROBLEM 5
+
+console.log("PROBLEM 5");
+for (var i = 3; i <= 15; i += 3){
+	console.log(i);
+}
+
+// PROBLEM 6
+
+console.log("PROBLEM 6");
+for (var i = 9; i >= 0; i -= 1){
+	console.log(i);
+}
+
+// PROBLEM 7
+
+console.log("PROBLEM 7");
+for (var i = 0; i <= 2; i += 1){
+	console.log(0);
+	console.log(1);
+	console.log(2);
+	console.log(3);
+}
